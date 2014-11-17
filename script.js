f37d2146dd4d868b6d619e46336f55f108b7da16
Milosz <miloszw@kth.se>
2014-11-17 13:17:56 +0100

    Added links and js script to obsfuscate my email address. Removed tooltip code.

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ $(document).ready(function() {
             duration: 1300,
         })
         .hover(function() {
-            $(this).toggleClass('grayscale', 1000);
+            $(this).toggleClass('grayscale');
         });
+
+    // Spam bots, go away!     
+    $('#email').click(function() {
+        document.location.href = "mailto:" + generate_address();
+    });
 });
+
+
+// security through obscurity
+var generate_address = function() {
+    var email1 = "zsolim";
+    var email2 = "kednoleiw@";
+    var el = [email1, email2];
+    var ret = "";
+    for (n = 0; n < el.length; n++) {
+        str = el[n];
+        for (i = str.length; i >= 0; i--) {
+            ret += str.charAt(i);
+        }
+    }
+    return ret + ".com"
+}
