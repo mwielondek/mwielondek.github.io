4d38b1613cdfa474192fe01c30c55911a1f3652e
Milosz <miloszw@kth.se>
2014-11-17 13:30:52 +0100

    Refined animations

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
     $('div#main img')
         .css('top', '-30px')
-        .animate({
-            opacity: 1,
-            top: 0
-        }, {
-            queue: false,
-            duration: 1300,
-        })
         .hover(function() {
             $(this).toggleClass('grayscale');
+        })
+        .each(function(index) {
+            $(this).delay(index*100).animate({
+                opacity: 1,
+                top: 0
+            }, {
+                queue: true,
+                duration: 400,
+            })
         });
 
     // Spam bots, go away!     
